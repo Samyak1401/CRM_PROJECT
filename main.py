a97c15ebc874d9ac420e7e32a54330ab67e79f17
Vaishnavishendkar <vaishnavi.shendkar28@gmail.com>
2026-06-19 10:55:09 +0530

    frontend added

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
         "http://127.0.0.1:5500",   
         "http://localhost:5500",   
         "http://localhost:3000",   
-        "crmproject-xtos8yfthvrddgxj3j5iip.streamlit.app",  #
+        "crmproject-tebky2zssvpxrs2smedmsc.streamlit.app",  #
     ],
     allow_credentials=True,
     allow_methods=["*"],
